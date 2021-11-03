d7e9a8ff3cee9333ba459d33264862c2791949df
SaotoGamer13 <82120589+SaotoGamer13@users.noreply.github.com>
2021-11-03 14:08:09 -0500

    Actualizacion de Pcomprado, agregado Pvendido

--- almacen/almacen/Pvendido.cs
+++ almacen/almacen/Pvendido.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,21 +6,19 @@ using System.Threading.Tasks;
 
 namespace almacen
 {
-    class Pcomprado
+    class Pvendido
     {
         private int precio;
         private DateTime fecha;
-        private List <Producto> producto = new List <Producto>();
+        
 
-        public Pcomprado(int precio, DateTime fecha, List<Producto> producto)
+        public Pvendido(int precio, DateTime fecha)
         {
             this.precio = precio;
             this.fecha = fecha;
-            this.producto = producto;
         }
 
         public int Precio { get => precio; set => precio = value; }
         public DateTime Fecha { get => fecha; set => fecha = value; }
-        public List <Producto> producto1 { get => producto; set => producto = value; }
     }
 }
