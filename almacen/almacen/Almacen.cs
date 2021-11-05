6126751714021a000448423f0d4edc08f6cd1360
dexfutur <68019118+dexfutur@users.noreply.github.com>
2021-11-04 20:23:09 -0500

    lo dije en wp

--- almacen/almacen/Almacen.cs
+++ almacen/almacen/Almacen.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 
 namespace almacen
 {
-    class Almacen
+    public class Almacen
     {
         private string nombre;
         private int id;
+        private List<Pkits> kits = new List<Pkits>();
         private List<Producto> productos = new List<Producto>();
         private List<Venta> ventas = new List<Venta>();
         private List<Compra> compras = new List<Compra>();
-        private List<Pkits> kits = new List<Pkits>();
 
         public Almacen(string nombre, int id, List<Producto> productos, List<Venta> ventas, List<Compra> compras, List<Pkits> kits)
         {
@@ -31,5 +31,150 @@ namespace almacen
         internal List<Venta> Ventas { get => ventas; set => ventas = value; }
         internal List<Compra> Compras { get => compras; set => compras = value; }
         internal List<Pkits> Kits { get => kits; set => kits = value; }
+
+        public static int AlmacenM()
+        {
+            Console.WriteLine("1. Agregar producto");
+            Console.WriteLine("2. agregar kit");
+            Console.WriteLine("3. lista de producto disponibles en bodega");
+            Console.WriteLine("4. list de kits disponibles en bodega");
+            Console.WriteLine("5. comprar producto");
+            Console.WriteLine("6. Comprar kit");
+            Console.WriteLine("7. vender producto");
+            Console.WriteLine("8. vender kit");
+            Console.WriteLine("9. lista de productos del cliente");
+            Console.WriteLine("10. lista de kits del cliente");
+            Console.WriteLine("11. generar reporte");
+
+            return Int32.Parse(Console.ReadLine());
+
+        }
+        static void Main(string[] args)
+        {
+            List<Pkits> kits = new List<Pkits>();
+            List<Producto> productos = new List<Producto>();
+            int valor = -1;
+
+            do
+            {
+                valor = AlmacenM();
+
+
+                switch (valor)
+                {
+                    case 1:
+                        Console.WriteLine(" ingresar nombre del Producto : ");
+                        string pnombre = Console.ReadLine();
+                        Console.WriteLine("ingresar codigo del producto");
+                        int pcodigo = int.Parse(Console.ReadLine());
+                        Console.WriteLine("ingresar precio del producto");
+                        int pprecio = int.Parse(Console.ReadLine());
+                        Console.WriteLine("elegir entre nacional e importada");
+                        bool pimportado = bool.Parse(Console.ReadLine());
+
+                        Producto producto = new Producto(pnombre, pcodigo, pprecio, pimportado);
+                        productos.Add(producto);
+
+                        break;
+
+                    case 2:
+                        Console.WriteLine(" ingresar nombre del kit");
+                        string knombre = Console.ReadLine();
+                        Console.WriteLine("ingresar codigo del kit");
+                        int kcodigo = int.Parse(Console.ReadLine());
+                        Console.WriteLine("ingresar precio del kit");
+                        int kprecio = int.Parse(Console.ReadLine());
+
+                        Pkits kit = new Pkits(knombre, kcodigo, kprecio);
+                        kits.Add(kit);
+                        break;
+
+                    case 3:
+
+                        foreach (Producto result in productos)
+                        {
+                            Console.WriteLine("===========================================");
+                            Console.WriteLine("informacion del producto:");
+                            Console.WriteLine("Nombre del producto: " + result.Pnombre);
+                            Console.WriteLine("Codigo del producto: " + result.Pcodigo);
+                            Console.WriteLine("Precio del producto" + result.Pprecio);
+                            Console.WriteLine("Importado? si.. Nacional? No: " + result.Pimportado);
+                            Console.WriteLine("=============================================");
+
+                        }
+                        break;
+                    case 4:
+                        foreach (Pkits result in kits)
+                        {
+                            Console.WriteLine("===========================================");
+                            Console.WriteLine("informacion del Kit:");
+                            Console.WriteLine("Nombre del Kit: " + result.Knombre1);
+                            Console.WriteLine("Codigo del Kit: " + result.Kcodigo1);
+                            Console.WriteLine("Precio del Kit: " + result.Kprecio1);
+                            Console.WriteLine("=============================================");
+
+                        }
+
+
+                        break;
+
+                    case 5:
+                        break;
+
+                    case 6:
+                        break;
+
+                    case 7:
+                        break;
+
+                    case 8:
+                        break;
+
+                    case 9:
+                        foreach (Producto result in productos)
+                        {
+                            Console.WriteLine("===========================================");
+                            Console.WriteLine("informacion del producto:");
+                            Console.WriteLine("Nombre del producto: " + result.Pnombre);
+                            Console.WriteLine("Codigo del producto: " + result.Pcodigo);
+                            Console.WriteLine("Precio del producto" + result.Pprecio);
+                            Console.WriteLine("Importado? si.. Nacional? No: " + result.Pimportado);
+                            Console.WriteLine("=============================================");
+
+                        }
+                        break;
+
+                    case 10:
+                        foreach (Pkits result in kits)
+                        {
+                            Console.WriteLine("===========================================");
+                            Console.WriteLine("informacion del Kit:");
+                            Console.WriteLine("Nombre del Kit: " + result.Knombre1);
+                            Console.WriteLine("Codigo del Kit: " + result.Kcodigo1);
+                            Console.WriteLine("Precio del Kit: " + result.Kprecio1);
+                            Console.WriteLine("=============================================");
+
+                        }
+                        break;
+
+                    case 11:
+                        break;
+
+
+                }
+
+
+            }
+            while (valor != 0);
+        }
+
+
     }
+
+
+
+   
+  
+   
 }
+
--- almacen/almacen/Pkits.cs
+++ almacen/almacen/Pkits.cs
@@ -9,6 +9,19 @@ namespace almacen
     class Pkits : Producto
     {
 
+        private string Knombre;
+        private int Kcodigo;
+        private int Kprecio;
 
+        public Pkits(string knombre, int kcodigo, int kprecio)
+        {
+            Knombre = knombre ?? throw new ArgumentNullException(nameof(knombre));
+            Kcodigo = kcodigo;
+            Kprecio = kprecio;
+        }
+
+        public string Knombre1 { get => Knombre; set => Knombre = value; }
+        public int Kcodigo1 { get => Kcodigo; set => Kcodigo = value; }
+        public int Kprecio1 { get => Kprecio; set => Kprecio = value; }
     }
 }
